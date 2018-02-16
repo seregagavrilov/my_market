1360527af2eb4967c6e668a24f4cd14273534495
Sergey Gavrilov <sergeigavrilov123@gmail.con>
2018-02-16 14:11:06 +0300

    fix code

--- app/routes.py
+++ app/routes.py
@@ -6,23 +6,16 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-
     items = []
-
     products = Product.query.all()
-
     for product in products:
-
         items.append(product)
-
     return render_template("index.html", items=items)
 
 
 @app.route("/product/<productid>")
 def product(productid):
-
     currentproduct = Product.query.filter_by(id=productid).first_or_404()
-
     return render_template("product.html", product=currentproduct, imgpath =currentproduct.imgpath)
 
 
